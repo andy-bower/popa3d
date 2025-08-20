354fe7792be0b54eede4ab8e2c714953ba007ce7
Andrew Bower <andrew@bower.uk>
2025-08-20 09:13:26 +0100

    Import IPv6 patch by Jun-ichiro itojun Hagino
    written against v0.5.1, applied against v0.5.0. Sourced from:
    https://www.openwall.com/popa3d/contrib/popa3d-0.5.1-ipv6-patch-1.diff.gz

--- standalone.c
+++ standalone.c
@@ -15,6 +15,7 @@
 #include <syslog.h>
 #include <time.h>
 #include <errno.h>
+#include <netdb.h>
 #include <sys/times.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -34,6 +35,7 @@ int deny_severity = SYSLOG_PRI_HI;
 extern int log_error(char *s);
 extern int do_pop_startup(void);
 extern int do_pop_session(void);
+extern int af;
 
 typedef sig_atomic_t a_int;
 typedef volatile a_int va_int;
@@ -45,7 +47,7 @@ typedef volatile a_int va_int;
  * information about sessions that we could have allowed to proceed.
  */
 static struct {
-	struct in_addr addr;		/* Source IP address */
+	char addr[NI_MAXHOST];		/* Source IP address */
 	a_int pid;			/* PID of the server, or 0 for none */
 	clock_t start;			/* When the server was started */
 	clock_t log;			/* When we've last logged a failure */
@@ -111,28 +113,56 @@ int main(void)
 {
 	int true = 1;
 	int sock, new;
-	struct sockaddr_in addr;
+	struct sockaddr_storage addr;
 	int addrlen;
 	int pid;
 	struct tms buf;
 	clock_t now;
 	int i, j, n;
+	struct addrinfo hints, *res;
+	char hbuf[NI_MAXHOST];
+	char sbuf[NI_MAXSERV];
+	int error;
 
 	if (do_pop_startup()) return 1;
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
 		return log_error("socket");
 
-	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
-	    (void *)&true, sizeof(true)))
-		return log_error("setsockopt");
+	snprintf(sbuf, sizeof(sbuf), "%u", DAEMON_PORT);
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_family = af;
+	hints.ai_flags = AI_PASSIVE;
+	error = getaddrinfo(NULL, sbuf, &hints, &res);
+	if (error)
+		return log_error("getaddrinfo");
 
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = inet_addr(DAEMON_ADDR);
-	addr.sin_port = htons(DAEMON_PORT);
-	if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)))
+	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sock < 0) {
+		freeaddrinfo(res);
+		return log_error("socket");
+	}
+
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
+	    (void *)&true, sizeof(true))) {
+		freeaddrinfo(res);
+		return log_error("setsockopt");
+	}
+
+#ifdef IPV6_V6ONLY
+	if (res->ai_family == AF_INET6 && setsockopt(sock, IPPROTO_IPV6,
+	    IPV6_V6ONLY, (void *)&true, sizeof(true))) {
+		freeaddrinfo(res);
+		return log_error("setsockopt");
+	}
+#endif
+
+	if (bind(sock, res->ai_addr, res->ai_addrlen)) {
+		freeaddrinfo(res);
 		return log_error("bind");
+	}
+	freeaddrinfo(res);
 
 	if (listen(sock, MAX_BACKLOG))
 		return log_error("listen");
@@ -170,6 +200,12 @@ int main(void)
 		addrlen = sizeof(addr);
 		new = accept(sock, (struct sockaddr *)&addr, &addrlen);
 
+		error = getnameinfo((struct sockaddr *)&addr, addrlen,
+		    hbuf, sizeof(hbuf), NULL, 0, NI_NUMERICHOST);
+		if (error)
+			; /* XXX */
+
+
 /*
  * I wish there was a portable way to classify errno's... In this case,
  * it appears to be better to risk eating up the CPU on a fatal error
@@ -189,9 +225,9 @@ int main(void)
 			if (sessions[i].pid ||
 			    (sessions[i].start &&
 			    now - sessions[i].start < MIN_DELAY * CLK_TCK)) {
-				if (sessions[i].addr.s_addr ==
-				    addr.sin_addr.s_addr)
-				if (++n >= MAX_SESSIONS_PER_SOURCE) break;
+				if (strcmp(sessions[i].addr, hbuf) == 0)
+					if (++n >= MAX_SESSIONS_PER_SOURCE)
+						break;
 			} else
 			if (j < 0) j = i;
 		}
@@ -202,7 +238,7 @@ int main(void)
 			    now - sessions[i].log >= MIN_DELAY * CLK_TCK) {
 				syslog(SYSLOG_PRI_HI,
 					"%s: per source limit reached",
-					inet_ntoa(addr.sin_addr));
+					hbuf);
 				sessions[i].log = now;
 			}
 			continue;
@@ -210,14 +246,13 @@ int main(void)
 
 		if (j < 0) {
 			syslog(SYSLOG_PRI_HI, "%s: sessions limit reached",
-				inet_ntoa(addr.sin_addr));
+				hbuf);
 			continue;
 		}
 
 		switch ((pid = fork())) {
 		case -1:
-			syslog(SYSLOG_PRI_ERROR, "%s: fork: %m",
-				inet_ntoa(addr.sin_addr));
+			syslog(SYSLOG_PRI_ERROR, "%s: fork: %m", hbuf);
 			break;
 
 		case 0:
@@ -226,7 +261,7 @@ int main(void)
 			check_access(new);
 #endif
 			syslog(SYSLOG_PRI_LO, "Session from %s",
-				inet_ntoa(addr.sin_addr));
+				hbuf);
 			if (dup2(new, 0) < 0) return log_error("dup2");
 			if (dup2(new, 1) < 0) return log_error("dup2");
 			if (dup2(new, 2) < 0) return log_error("dup2");
@@ -234,7 +269,8 @@ int main(void)
 			return do_pop_session();
 
 		default:
-			sessions[j].addr = addr.sin_addr;
+			strlcpy(sessions[j].addr, hbuf,
+				sizeof(sessions[j].addr));
 			(va_int)sessions[j].pid = pid;
 			sessions[j].start = now;
 			sessions[j].log = 0;
--- startup.c
+++ startup.c
@@ -6,6 +6,7 @@
 
 #if POP_OPTIONS
 
+#include <sys/socket.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +25,8 @@ extern char *__progname;
 static char *progname;
 #endif
 
+int af = AF_INET;
+
 static void usage(void)
 {
 	fprintf(stderr, "Usage: %s [-D]\n", progname);
@@ -40,10 +43,16 @@ int main(int argc, char **argv)
 		progname = POP_SERVER;
 #endif
 
-	while ((c = getopt(argc, argv, "D")) != -1) {
+	while ((c = getopt(argc, argv, "D46")) != -1) {
 		switch (c) {
 		case 'D':
 			standalone++;
+			break;
+		case '4':
+			af = AF_INET;
+			break;
+		case '6':
+			af = AF_INET6;
 			break;
 
 		default:
